e8eac33ab5d54968f0d7d069e889ef273e10d1ae
Carrie <carriearoberts95@gmail.com>
2021-09-14 13:25:03 +0100

    Committing my solutions to exercises

--- src/challenges/0b-object-destructuring.ts
+++ src/challenges/0b-object-destructuring.ts
@@ -1,4 +1,5 @@
 const country = {
+  //properties, key value pairs
   name: "United Kingdom",
   abbreviation: "UK",
   capital: "London",
@@ -6,9 +7,14 @@ const country = {
 };
 
 // TODO: use object destructuring
-const abbreviation = country.abbreviation;
-const capital = country.capital;
-const population = country.population;
+// const abbreviation = country.abbreviation;
+// const capital = country.capital;
+// const population = country.population;
+
+const { abbreviation, capital, population } = country
+//removes the need for country.prop or country.population 
+//country is an object, we want to get the values out without acessing it via the object
+// not always necessary but useful
 
 // export so the values can be tested - but you can ignore this
 export { abbreviation, capital, population };
